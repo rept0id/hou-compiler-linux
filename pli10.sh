88ef40522e50482765f9aa298e627e36ffb2af0a
Rantouan Achmet <rad@simplecode.gr>
2025-03-14 16:52:41 +0200

    Update pli10.sh

--- pli10.sh
+++ pli10.sh
@@ -2,56 +2,148 @@
 
 SRC_FILENAME=$1
 
+SRC_TMP="/tmp/$$"
+
 WINEPREFIX="$(pwd)/wine32"
 PROJECT_DIR="$(pwd)"
 
-# init
+SRC_BUFFER="$SRC_TMP/buffer.txt"
 
-function init() {
+### # # ###
 
-    # init : deps
+function prepare() {
+    echo -e "\n[hou-compiler-linux] prepare..."
+
+    # prepare : deps
 
     sudo dpkg --add-architecture i386
     sudo apt update
     sudo apt install wine32
 
-    # init : wine
+    # prepare : wine
 
     mkdir -p "$WINEPREFIX"
 
     if [ ! -d "$WINEPREFIX/drive_c" ]; then
-        echo "Initializing 32-bit Wine prefix in $WINEPREFIX..."
+        echo "Preparing 32-bit Wine prefix in $WINEPREFIX..."
         WINEARCH=win32 WINEPREFIX="$WINEPREFIX" winecfg
     fi
 
-    # init : wine : TDM-GCC-32
+    # prepare : wine : TDM-GCC-32
 
-    if [ ! -d "$WINEPREFIX/drive_c/TDM-GCC-32" ]; 
-    then     
+    if [ ! -d "$WINEPREFIX/drive_c/TDM-GCC-32" ];
+    then
         WINEPREFIX="$WINEPREFIX" wine tdm-gcc-10.3.0.exe
 
-        if [ ! -d "$WINEPREFIX/drive_c/TDM-GCC-32" ]; 
-        then     
+        if [ ! -d "$WINEPREFIX/drive_c/TDM-GCC-32" ];
+        then
             echo "Please install manually TDM-GCC under current wine installation : wine/drive_c/TDM-GCC-32";
         fi
     fi
 
-    # init : .inited file
+    # prepare : .prepared file
 
-    touch .inited
+    touch .prepared
 }
 
-if [ ! -e .inited ]; then
+function init() {
+    echo -e "\n[hou-compiler-linux] init..."
+
+    ### # # ###
+
+    mkdir -p "$SRC_TMP"
+
+    touch $SRC_BUFFER
+}
+
+### # # ###
+
+function transpose() {
+    echo -e "\n[hou-compiler-linux] transpose..."
+
+    ### # # ###
+
+    iconv -f UTF-8 -t CP1253 ./${SRC_FILENAME}.utf8.eap > ./${SRC_FILENAME}.cp1253.eap
+}
+
+function compile() {
+    echo -e "\n[hou-compiler-linux] compile..."
+
+    ### # # ###
+
+    echo | WINEPREFIX="$WINEPREFIX" wine cmd /c "chcp 1253 && pli10.exe ${SRC_FILENAME}.cp1253.eap" | iconv -f CP1253 -t UTF-8
+}
+
+function run() {
+    echo -e "\n[hou-compiler-linux] run..."
+
+    ### # # ###
+
+    echo -n '' > $SRC_BUFFER
+
+    ### # # ###
+
+    LANG=el_GR.CP-1253 WINEPREFIX="$WINEPREFIX" wine cmd /c "chcp 1253 && ${SRC_FILENAME}.cp1253.eap.exe" | tee $SRC_BUFFER > /dev/null &
+
+    WINE_PID=$!
+
+    ### # # ###
+
+    while true; do
+        if [ -s $SRC_BUFFER ]; then
+            cat $SRC_BUFFER | iconv -f CP1253 -t UTF-8
+
+            echo -n '' > $SRC_BUFFER
+        fi
+
+        ### # # ###
+
+        if ! ps -p $WINE_PID > /dev/null; then
+            break
+        fi
+
+        ### # # ###
+
+        sleep 0.5
+    done
+}
+
+function clean() {
+    echo -e "\n[hou-compiler-linux] clean..."
+
+    ### # # ###
+
+    rm -rf $SRC_TMP
+}
+
+### # # ###
+
+function main() {
+    # prepare
+    if [ ! -e .prepared ]; then
+        prepare
+    fi
+
+    # init
     init
-fi
 
-# encode
-iconv -f UTF-8 -t cp1253 ./${SRC_FILENAME}.utf8.eap > ./${SRC_FILENAME}.cp1253.eap
+    ### # # ###
 
-# compile
-#
-# the reversed pipe to echo is used to print the compiler's output through iconv
-echo | WINEPREFIX="$WINEPREFIX" wine cmd /c "pli10.exe ${SRC_FILENAME}.cp1253.eap" | iconv -f cp1253 -t UTF-8
+    # transpose
+    transpose
 
-# run
-WINEPREFIX="$WINEPREFIX" wine cmd /c "${SRC_FILENAME}.cp1253.eap.exe"
+    # compile
+    compile
+
+    # run
+    run
+
+    # clean
+    clean
+
+    ### # # ###
+
+    echo -e "\n[hou-compiler-linux] exit..."
+}
+
+main
